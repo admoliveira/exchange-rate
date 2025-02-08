e48cca005020e6abe6063b5114b113042df9fce3
Alexandre Oliveira <alexandredmoliveira@gmail.com>
2025-02-08 15:35:18 +0000

    feat: add keycloak

--- src/main/java/com/admoliveira/exchangerate/configuration/SecurityConfig.java
+++ src/main/java/com/admoliveira/exchangerate/configuration/SecurityConfig.java
@@ -1,33 +1,35 @@
 package com.admoliveira.exchangerate.configuration;
 
-import com.admoliveira.exchangerate.filter.ApiKeyAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final ApiKeyAuthenticationFilter apiKeyAuthenticationFilter;
-
-    public SecurityConfig(ApiKeyAuthenticationFilter apiKeyAuthenticationFilter) {
-        this.apiKeyAuthenticationFilter = apiKeyAuthenticationFilter;
-    }
+    public static final String SCOPE_GET_RATES = "get-rates";
+    public static final String SCOPE_GET_CONVERSIONS = "get-conversions";
+    public static final String SCOPE_GRAPHQL = "graphql";
 
     @Bean
-    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(CorsConfigurer::disable)
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/rates").authenticated()
-                            .requestMatchers("/conversions").authenticated()
-                            .requestMatchers("/graphql").authenticated()
-                            .anyRequest().permitAll())
-             .addFilterBefore(apiKeyAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                        auth.requestMatchers("/rates").hasAuthority("SCOPE_" + SCOPE_GET_RATES)
+                                .requestMatchers("/conversions").hasAuthority("SCOPE_" + SCOPE_GET_CONVERSIONS)
+                                .requestMatchers("/graphql").hasAuthority("SCOPE_" + SCOPE_GRAPHQL)
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
         return http.build();
     }
+
 }
--- src/main/java/com/admoliveira/exchangerate/configuration/SwaggerConfig.java
+++ src/main/java/com/admoliveira/exchangerate/configuration/SwaggerConfig.java
@@ -2,26 +2,43 @@ package com.admoliveira.exchangerate.configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+import static com.admoliveira.exchangerate.configuration.SecurityConfig.SCOPE_GET_CONVERSIONS;
+import static com.admoliveira.exchangerate.configuration.SecurityConfig.SCOPE_GET_RATES;
+
+
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${exchange-rate.swagger-config.issuer-uri}")
+    private String issuerUri;
+
     @Bean
     public OpenAPI springOpenApiConfig() {
+        final  Scopes scopes = new Scopes();
+        scopes.put(SCOPE_GET_RATES, "Call get rates");
+        scopes.put(SCOPE_GET_CONVERSIONS, "Call get conversions");
+
         return new OpenAPI()
                 .components(new Components()
-                        .addSecuritySchemes("api_key",
+                        .addSecuritySchemes("client_credentials",
                                 new SecurityScheme()
-                                        .type(SecurityScheme.Type.APIKEY)
-                                        .description("Api Key")
-                                        .in(SecurityScheme.In.HEADER)
-                                        .name("X-API-Key")))
-                .security(List.of(new SecurityRequirement().addList("api_key")));
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .flows(new OAuthFlows()
+                                                .clientCredentials(new OAuthFlow()
+                                                        .scopes(scopes)
+                                                        .tokenUrl(issuerUri)))))
+                .security(List.of(new SecurityRequirement().addList("client_credentials")));
     }
+
 }
